e36244dd45f1fd5966e91e750cca803118e0f6fa
Nathaniel Roberts <nathanielroberts@uvic.ca>
2022-08-15 02:46:58 -0230

    basic react app

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const config = {
+    entry: path.join(__dirname, "app/client/", "index.js"),
+    mode: 'development',
+    output: {
+        path:path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'] 
+                    }
+                }
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "app/client/", "index.html"),
+        }),
+    ],
+}
+
+export default config;
