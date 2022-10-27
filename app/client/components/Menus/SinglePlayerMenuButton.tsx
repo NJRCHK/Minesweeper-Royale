f915b659558bdffc72b54bfb962aea58532200e6
NJRCHK <nathanieljroberts11@gmail.com>
2022-10-27 12:44:08 -0700

    changed main menu styling

--- app/client/components/Menus/SinglePlayerMenuButton.tsx
+++ app/client/components/Menus/SinglePlayerMenuButton.tsx
@@ -26,16 +26,22 @@ export default function SinglePlayerMenuButton(props: SinglePlayerMenuButtonProp
     function showSubMenu(): void {
         setOptionsShown(optionsShown => !optionsShown);
     }
+    function hideSubMenu() {
+        setOptionsShown(false);
+    }
 
     return (
-        <div>
-            <div onClick={showSubMenu}>Singleplayer</div>
-            {optionsShown &&         
+        <div onClick={showSubMenu} onMouseLeave={hideSubMenu} className='main-menu-subitem'>
+            <div>
+                {!optionsShown && <div>Singleplayer</div>}
+                {optionsShown &&         
                 <div>
-                <button onClick={() => {props.startSinglePlayerGame(beginnerConfig)}}>Beginner</button>
-                <button onClick={() => {props.startSinglePlayerGame(intermediateConfig)}}>Intermediate</button>
-                <button onClick={() => {props.startSinglePlayerGame(expertConfig)}}>Expert</button>
-            </div>}
+                    <button className='singleplayer-submenu-button' onClick={() => {props.startSinglePlayerGame(beginnerConfig)}}>Beginner</button>
+                    <button className='singleplayer-submenu-button' onClick={() => {props.startSinglePlayerGame(intermediateConfig)}}>Intermediate</button>
+                    <button className='singleplayer-submenu-button' onClick={() => {props.startSinglePlayerGame(expertConfig)}}>Expert</button>
+                </div>
+                }
+            </div>
         </div>
     )
 }
