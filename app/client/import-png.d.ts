88ab352655b5c9053b0e0beba688c1df349f68a9
NJRCHK <nathanieljroberts11@gmail.com>
2023-01-24 22:55:43 -0800

    fixed typescript image import issue

--- app/client/import-png.d.ts
+++ app/client/import-png.d.ts
@@ -1,4 +1,20 @@
-declare module "*.png";
-declare module "*.svg";
-declare module "*.jpeg";
-declare module "*.jpg";
+declare module "*.png" {
+    const value: any; 
+    export = value;
+}
+
+declare module "*.svg"{
+    const value: any;
+    export = value;
+}
+
+declare module "*.jpeg" {
+    const value: any;
+    export = value;
+}
+
+declare module "*.jpg" {
+    const value: any;
+    export = value;
+}
+
